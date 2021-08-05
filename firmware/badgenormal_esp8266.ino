7a85ef853da513af45db059cead4b697074515ea
Daniel Nduati <dannduati2@gmail.com>
2021-08-05 18:59:26 +0300

    Updated the normal board firmware
    Fixed the espnow and ap conflicts

--- firmware/badgenormal_esp8266.ino
+++ firmware/badgenormal_esp8266.ino
@@ -1,14 +1,13 @@
+//This is the normal board
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <espnow.h>
-#include "index_normal.h" //Our HTML webpage contents
+#include "names.h"
+#include "index_normal.h" //our webpage
 
-
-
-//board ap config
-const char* ssid = "NormalBadge";
-const char* password = "dandandandan";
+/* Put your SSID & Password */
+const char* ssid = "NormalBoard";  // Enter SSID here
+const char* password = "12345678";  //Enter Password here
 
 //device IP
 IPAddress local_ip(192, 168, 1, 123);
@@ -31,40 +30,45 @@ typedef struct struct_message {
 struct_message incomingData;
 
 long lastSendTime = 0;        // last send time
-int interval = 10000;          // interval between name prints
+int interval = 40000;          // interval between name prints
 
 int special_message_count = 0;
 String special_messages[4]; //array to store special messages obtained
 
 
-void add_special_name(char spec_name[40]) {
+void add_special_name(char spec_name[40]){
   String special = spec_name;
   bool match = false;
   //check if the special name is in the array
-  for (int i = 0; i < 4; i++) {
-    if (special.equals(special_messages[i])) {
+  for (int i = 0; i<4; i++){
+    if (special.equals(special_messages[i])){
       match = true;
       break;
     }
   }
-  if (!match) {
+  if (!match){
     //check the number of elements currently in the array
-    //this is a quick and dirty method
+    //this is a quick and dirty method 
     int numberOfElements = 0;
-    for (int i = 0; i < 4; i++) {
-      if (special_messages[i] != 0) {
+    for (int i =0; i<4; i++){
+      if (special_messages[i] !=0){
         numberOfElements++;
       }
-      else {
+      else{
         break;
       }
     }
     //add match
     special_messages[numberOfElements] = special;
   }
-  
+  //print special names
+  Serial.println("Special names obtained: ");
+  for(int i=0; i<4; i++){
+    Serial.print(i+1);
+    Serial.print(": ");
+    Serial.println(special_messages[i]);
+  }
 }
-
 // Callback when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *receivedData, uint8_t len) {
   memcpy(&incomingData, receivedData, sizeof(incomingData));
@@ -78,19 +82,13 @@ void OnDataRecv(uint8_t * mac, uint8_t *receivedData, uint8_t len) {
   add_special_name(incomingData.special_name);
 }
 
-void setup(void) {
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_AP_STA);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   WiFi.softAP(ssid);
-
-  delay(100);
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  // Print ESP8266 Local IP Address
-  Serial.println(WiFi.localIP());
-
   // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
@@ -99,7 +97,6 @@ void setup(void) {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
-  // Wait for connection
 
   server.on("/", handleRoot);      //Which routine to handle at root location
 
@@ -107,7 +104,17 @@ void setup(void) {
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+void loop() {
   server.handleClient();          //Handle client requests
-  
+  if (millis() - lastSendTime > interval) {
+
+    //display the list of names on the serial monitor
+    Serial.println("------------------------------------");
+    Serial.println("          Never forgotten           ");
+    Serial.println("------------------------------------");
+    for (int i = 0; i < 297; i++) {
+      Serial.println(names_to_be_displayed[i]);
+    }
+    lastSendTime = millis();
+  }
 }
